d444086d0ac06e6ace426ec6524f8fe89d9d91a7
gotuiyan <gto.rikujyo@icloud.com>
2024-02-16 19:05:42 +0900

    added __init__.py

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .gector import *
--- gector/__init__.py
+++ gector/__init__.py
@@ -2,7 +2,7 @@ from .modeling import GECToR
 from .configuration import GECToRConfig
 from .dataset import load_dataset, GECToRDataset
 from .predict import predict, load_verb_dict
-from gector.predict_verbose import predict_verbose
+from .predict_verbose import predict_verbose
 from .vocab import (
     build_vocab,
     load_vocab_from_config,
